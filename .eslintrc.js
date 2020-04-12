b9b70548f80fdef6b53234381f9955676a01ef7e
Carlos Perilla <carlos.perilla@valkertown.org>
2020-04-12 17:10:36 -0500

    Update eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    "no-prototype-builtins": 0,
+    "no-undef" : 0,
+    "no-unused-vars" : 0
+    }
+};
